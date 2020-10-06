4048bf0ac78ce0194a5c4018e3e20e78fe99e450
Dorin <mirondorin99@gmail.com>
2020-10-06 19:26:45 +0300

    Added project

---
+++ Assets/Scripts/AttackAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackAnimation : MonoBehaviour
+{
+
+    public Camera arCamera;
+    private Animator animator;
+    float dist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(arCamera.GetComponent<Transform>().position, gameObject.GetComponent<Transform>().position);
+
+        if (dist <= 200.0f)
+        {
+            animator.SetBool("Attacking", true);
+            animator.SetBool("Skip", false);
+        }
+        else
+        {
+            animator.SetBool("Attacking", false);
+            animator.SetBool("Skip", true);
+        }
+        Debug.Log(dist);
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public GameObject cube;
+    private Transform cubeTransform;
+    private float zPos = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeTransform = cube.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubeTransform.position = new Vector3(0, 0, 1);
+        zPos += 0.1f;
+    }
+}
